1d81d2a489a6c3c2d5c5d08c9093171f5414a029
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-01-26 06:42:55 +0300

    Добавляем предварительные размеры установки

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -7,12 +7,25 @@
 
 
 #include <G4VUserDetectorConstruction.hh>
-
+#include "G4SystemOfUnits.hh"
 
 
 class DetectorConstruction : public G4VUserDetectorConstruction{
 public:
     G4VPhysicalVolume *Construct() override;
+
+    const double detector_side_size = 0.5*meter;
+    const double detector_length = 2*meter;
+    const double calorimeter_lead_thickness = 1*cm;
+    const double calorimeter_plastic_thickness = 1*cm;
+    const int number_of_tracking_cell = 10;
+    const double tracking_cell_size = 0.05*meter;
+    const double tracking_thickness = 0.5*mm;
+    const double distance_tracking_layer = 0.10*meter;
+    const double distance_tracking_area = 0.20*meter;
+    const double distance_tracking_center = 0.20*meter;
+    const double distance_tracking_calorimeter = 0.10*meter;
+
 
 };
 
