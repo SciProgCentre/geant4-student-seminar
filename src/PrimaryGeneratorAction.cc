e81332f016e79e3ef8b7e3e228c228a9e41027ce
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-04-15 11:11:41 +0300

    Merge branch 'master' of /home/zelenyy/npm/git/geant4-student-seminar with conflicts.

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -6,53 +6,30 @@
 #include <G4Electron.hh>
 #include "PrimaryGeneratorAction.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4ParticleTable.hh"
 
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent) {
+/**
+ * TODO()
+ * Ваша задача, используя код который вы написали на прошлых занятиях,
+ * генерировать начальные частицы возникающие при распаде Pi мезона.
+ */
 
-    if (isPiDecay) {
-        // Используем генератор распада Пи-мезона
-        bool flag = false;
-        std::vector<Particle> particle;
-        do {
-            flag = false;
-            particle = piDecay->DecayPi();
-            for (auto it: particle) {
-                auto momentum = it.momentum;
-                if ((momentum.theta() / radian > 0.1 && momentum.theta() / radian < pi - 0.1)) {
-                    flag = true;
-                }
-            }
-        } while (flag);
+    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
+    fParticleGun->SetParticleEnergy(60.0 * MeV);
 
-        for (auto it : particle) {
-            fParticleGun->SetParticleMomentumDirection(it.momentum);
-            fParticleGun->SetParticleEnergy(it.energy);
-            auto defenition = particleTable->FindParticle(it.pdgid);
-            fParticleGun->SetParticleDefinition(defenition);
-            fParticleGun->GeneratePrimaryVertex(anEvent);
-        }
-    } else {
-        fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
-        fParticleGun->SetParticleEnergy(60.0 * MeV);
 
-        // Генерируем первую первичную частицы для события
-        fParticleGun->SetParticleDefinition(G4Electron::Definition());
-        fParticleGun->GeneratePrimaryVertex(anEvent);
+    // Генерируем первую первичную частицы для события
+    fParticleGun->SetParticleDefinition(G4Electron::Definition());
+    fParticleGun->GeneratePrimaryVertex(anEvent);
 
-        // Генерируем вторую первичную частицу для события
-        fParticleGun->SetParticleDefinition(G4Gamma::Definition());
-        fParticleGun->GeneratePrimaryVertex(anEvent);
-    }
-
+    // Генерируем вторую первичную частицу для события
+    fParticleGun->SetParticleDefinition(G4Gamma::Definition());
+    fParticleGun->GeneratePrimaryVertex(anEvent);
 }
 
-PrimaryGeneratorAction::PrimaryGeneratorAction(bool isPiDecay) : G4VUserPrimaryGeneratorAction(), isPiDecay(isPiDecay) {
+PrimaryGeneratorAction::PrimaryGeneratorAction() : G4VUserPrimaryGeneratorAction() {
 
     G4int n_particle = 1;
     fParticleGun = new G4ParticleGun(n_particle);
 
-    piDecay = new PiDecayGenerator();
-
-    particleTable = G4ParticleTable::GetParticleTable();
 }
